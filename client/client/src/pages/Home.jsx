8f8704d101e38fc9a20465371195fe90d72262fb
meelan10 <milanwagle73@gmail.com>
2026-07-11 22:52:00 +0545

    updated ui

--- client/client/src/pages/Home.jsx
+++ client/client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/layout/Navbar";
+import Navbar from "../components/layouts/Navbar";
 import Hero from "../components/home/Hero";
 import Trusted from "../components/home/Trusted";
 
